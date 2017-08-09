9c2a6aa608deb1cb6d7c842ad07f009e22a28d69
Kristóf <illeskristof1994@gmail.com>
2017-08-09 15:30:19 +0200

    fckin genius

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+import csv
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -6,10 +7,11 @@ app.secret_key = "hello"
 
 @app.route('/')
 def route_list():
-    note_text = None
-    if 'note' in 'list.csv':
-        note_text = 'list.csv'['note']
-    return render_template('list.html', note=note_text)
+    with open('list.csv', 'r') as listForms:
+        reader = csv.reader(listForms)
+        readlist = [row for row in reader]
+    print(readlist)
+    return render_template('list.html', readlist=readlist)
 
 
 @app.route('/story')
@@ -20,10 +22,18 @@ def route_edit():
     return render_template('form.html', note=note_text)
 
 
-@app.route('/save-note', methods=['POST'])
+@app.route('/save', methods=['POST'])
 def route_save():
-    print('POST request received!')
-    'list.csv'['note'] = request.form['note']
+    Story_title = request.form['Story title']
+    User_Story = request.form['User Story']
+    Acceptance_criteria = request.form['Acceptance criteria']
+    Business_value = request.form['Business value']
+    Estimation = request.form['Estimation']
+    Status = request.form['Status']
+    
+    f = open('list.csv', 'a')
+    f.write(','.join([Story_title, User_Story, Acceptance_criteria, Business_value, Estimation, Status])+'\n')
+    f.close()
     return redirect('/')
 
 
